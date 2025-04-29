f8227b49f94508f68d3732097ea741567df4707f
yasutakatou <fzk01325@nifty.com>
2025-04-29 14:50:10 +0900

    first

---
+++ go.mod
@@ -0,0 +1,14 @@
+module ds18b20
+
+go 1.19
+
+require (
+	github.com/mark3labs/mcp-go v0.23.1
+	github.com/yryz/ds18b20 v0.0.0-20200527154408-4a8f84bb82d4
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "context"
+    "fmt"
+
+    "github.com/mark3labs/mcp-go/mcp"
+    "github.com/mark3labs/mcp-go/server"
+	"github.com/yryz/ds18b20"
+)
+
+//npx @modelcontextprotocol/inspector --config ./config.json --server ds18b20
+
+func main() {
+	sensors, err := ds18b20.Sensors()
+    if err != nil {
+        panic(err)
+    }
+
+    s := server.NewMCPServer("MCP温度計", "0.0.1")
+
+    currentTimeTool := mcp.NewTool("temperature",
+        mcp.WithDescription("現在温度を返します"),
+    )
+
+    s.AddTool(currentTimeTool, func(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+		for _, sensor := range sensors {
+			t, err := ds18b20.Temperature(sensor)
+			if err == nil {
+				message := fmt.Sprintf("temperature: %.2f°C", t)
+				return mcp.NewToolResultText(message), nil
+			}
+		}
+		return mcp.NewToolResultText("no sensor found."), nil
+    })
+
+    if err := server.ServeStdio(s); err != nil {
+        fmt.Printf("サーバーエラー: %v\n", err)
+    }
+}
